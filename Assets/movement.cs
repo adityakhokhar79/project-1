6fab81c116e27a029ec334b8f05767ae7c1323dd
adityakhokhar79 <75921728+adityakhokhar79@users.noreply.github.com>
2020-12-15 12:34:57 +0530

    2

---
+++ Assets/movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+   
+    public Rigidbody bot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w")) 
+        {
+            bot.AddForce(0,0,1000*Time.deltaTime);
+
+        }
+        if (Input.GetKey("a"))
+        {
+            bot.AddForce(  -1000*Time.deltaTime,0,0);
+
+        }
+        if (Input.GetKey("d"))
+        {
+            bot.AddForce(1000 * Time.deltaTime,0,0);
+        }
+        if (Input.GetKey("s"))
+        {
+            bot.AddForce(0,0,-1000 * Time.deltaTime);
+
+        }
+    }
+}
